078775263f03496974f061e1e958f34ec54c10a1
petalpurposed <136622127+petalpurposed@users.noreply.github.com>
2023-06-15 23:25:10 +0530

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-const hamMenu = document.getElementById('ham-menu');
-        const navBar = document.getElementById('nav-bar');
-    
-        hamMenu.addEventListener('click', function () {
-          navBar.classList.toggle('active');
-        });
